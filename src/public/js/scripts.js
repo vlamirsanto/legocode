aaeef40ab5d391879f89641e05e28d7d8eb61a7c
Vlamir Santo <vlamir.santo@dotlib.com.br>
2019-11-26 18:05:47 -0300

    Implementando telas

--- src/public/js/scripts.js
+++ src/public/js/scripts.js
@@ -1,16 +1,24 @@
 $(document).ready(function() {
-  var codeInserted = `<textarea class="form-control"></textarea>
-<div class="input-group">
+  var html = {
+    textarea: `<textarea class="form-control"></textarea>`,
+    input: `
   <input
     type="text"
     class="form-control"
     placeholder="Digite sua mensagem"
   />
-  <span class="input-group-btn">
+`,
+    button: `<span class="input-btn">
     <button class="btn btn-success" type="button">Enviar</button>
-  </span>
-</div>`;
-  $("#code").html(codeInserted);
+  </span>`
+  };
+
+  $(".btn-insert").click(function() {
+    var rel = $(this).attr("rel");
+    var value = $("#code").val();
+
+    $("#code").html(value + html[rel]);
+  });
 
   $("#renderCode").click(function() {
     var code = $("#code").val();
